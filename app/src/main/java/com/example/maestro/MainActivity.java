6acade175932196b5a7989b8491b72c32181ba65
agnieszka <agnieszka.hamal@gmail.com>
2023-04-01 16:42:05 +0200

    działające ikony przejscia do innych ekranów oraz zmiana koloru aplikacji.

--- app/src/main/java/com/example/maestro/MainActivity.java
+++ app/src/main/java/com/example/maestro/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.maestro;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageButton = (ImageButton) findViewById(R.id.btn_attendance);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AttendanceActivity.class);
+                startActivity(intent);
+            }
+        });
+        imageButton= (ImageButton) findViewById(R.id.btn_calendar);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
+        imageButton= (ImageButton) findViewById(R.id.btn_instrument);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, InstrumentActivity.class);
+                startActivity(intent);
+            }
+        });
+        imageButton= (ImageButton) findViewById(R.id.btn_notes);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NotesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
