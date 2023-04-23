c74a683e6f2ecfb1e81eb85086756b857fbad5ab
agnieszka <agnieszka.hamal@gmail.com>
2023-04-23 18:19:36 +0200

    dodanie do DbHelper funkcji umozliwiajacych operaje na bazie

--- app/src/main/java/com/example/maestro/DbHelper.java
+++ app/src/main/java/com/example/maestro/DbHelper.java
@@ -1,6 +1,14 @@
 package com.example.maestro;
 
+import static com.example.maestro.RegistrationModel.COLUMN_EMAIL;
+import static com.example.maestro.RegistrationModel.COLUMN_ID;
+import static com.example.maestro.RegistrationModel.COLUMN_LOGIN;
+import static com.example.maestro.RegistrationModel.COLUMN_PASSWORD;
+import static com.example.maestro.RegistrationModel.TABLE_NAME;
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -29,6 +37,54 @@ public class DbHelper extends SQLiteOpenHelper {
         // Aktualizacja bazy danych
         db.execSQL("DROP TABLE IF EXISTS registration");
         onCreate(db);
+    }
+
+
+    public boolean addUser(RegistrationModel registrationModel) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues Values = new ContentValues();
+        Values.put(COLUMN_LOGIN, registrationModel.getLogin());
+        Values.put(COLUMN_PASSWORD, registrationModel.getPassword());
+        Values.put(COLUMN_EMAIL,registrationModel.getEmail());
+        long insert = db.insert(TABLE_NAME, null, Values);
+        if (insert == -1) {
+            return false;
+
+        } else {
+            return true;
+        }
+    }
+
+    public RegistrationModel getModelByID(int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String queryString = " SELECT * FROM " + TABLE_NAME + " WHERE " + COLUMN_ID + " = " + id;
+        Cursor cursor = db.rawQuery(queryString, null);
+        if (cursor.moveToFirst()) {
+            int column_id = cursor.getInt(0);
+            String login = cursor.getString(1);
+            String password = cursor.getString(2);
+            String email= cursor.getString(3);
+            RegistrationModel Register = new RegistrationModel(column_id, login,password, email);
+            return Register;
+        }
+        cursor.close();
+        db.close();
+        return null;
+    }
+
+    public boolean deleteUsersByID(int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, COLUMN_ID + "=" + id, null) > 0;
+    }
+
+
+    public boolean updateUsers(RegistrationModel registrationModel) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues Values = new ContentValues();
+        Values.put(COLUMN_LOGIN, registrationModel.getLogin());
+        Values.put(COLUMN_PASSWORD, registrationModel.getPassword());
+        Values.put(COLUMN_EMAIL,registrationModel.getEmail());
+        return db.update(TABLE_NAME, Values, COLUMN_ID + "= ?", new String[]{String.valueOf(registrationModel.getId())}) > 0;
     }
 }
 
