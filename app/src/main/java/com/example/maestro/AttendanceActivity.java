40d08643165b3e18920aae7e33090a511dc960c1
Olas279 <olekkus@onet.pl>
2023-05-08 16:09:29 +0200

    Zmiany w kalendarzu/terminarzu

--- app/src/main/java/com/example/maestro/AttendanceActivity.java
+++ app/src/main/java/com/example/maestro/AttendanceActivity.java
@@ -1,16 +1,32 @@
 package com.example.maestro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AttendanceActivity extends AppCompatActivity {
     private MyOrientationEventListener mOrientationEventListener;
+    private RecyclerView activityList;
+    private Button markAttendanceButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance);
         mOrientationEventListener = new MyOrientationEventListener(this);
+
+        activityList = findViewById(R.id.activity_list);
+        markAttendanceButton = findViewById(R.id.mark_attendance_button);
+
+        markAttendanceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // code for marking attendance
+
+            }
+        });
     }
     protected void onResume() {
         super.onResume();
